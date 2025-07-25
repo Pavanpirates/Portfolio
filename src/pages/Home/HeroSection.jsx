027dfc14e2c373bbbf09e6dbff80aa7ac5f197d8
Sanku Pavan Kumar <sankupavanraj@gmail.com>
2025-07-25 15:36:50 +0530

    Global Css

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,6 +1,32 @@
 export default function HeroSection() {
   return(
-    <h1>HeroSection</h1>
+    <section id= "heroSection" className="hero--section">
+      <div classname= "hero--section--content--box">
+        <div clasname= "hero--section--content">
+          <p className="section--title">Hey,I,am Pavan</p>
+          <h1 className="hero--section--title">
+             <span className="hero--section--title--color">
+                Full Stack 
+                </span> {" "}
+                <br />
+                Developer
+          </h1>
+               <p className="hero--section--description">
+                I am a Full Stack Developer with a passion for building
+                <br />
+                 web applications that provide seamless user experiences.
+               </p>
+        </div>
+          <button className="btn btn-primary">
+            Get In Touch
+          </button>
+      </div>
+      <div clasname="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+
+      </div>
+      
+       </section>
   );
   
 }
